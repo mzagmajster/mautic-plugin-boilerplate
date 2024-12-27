374d653e0af20f58cdfba5feb0ed10f5118c3162
Matic Zagmajster <maticzagmajster@gmail.com>
2024-12-27 19:53:14 +0100

    Extend AbstractPluginBundle

--- HelloWorldBundle.php
+++ HelloWorldBundle.php
@@ -4,37 +4,8 @@ declare(strict_types=1);
 
 namespace MauticPlugin\HelloWorldBundle;
 
-use Doctrine\DBAL\Schema\Schema;
-use Mautic\CoreBundle\Factory\MauticFactory;
-use Mautic\PluginBundle\Bundle\PluginBundleBase;
-use Mautic\PluginBundle\Entity\Plugin;
+use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
 
-class HelloWorldBundle extends PluginBundleBase
+class HelloWorldBundle extends AbstractPluginBundle
 {
-    /**
-     * Called by PluginController::reloadAction when adding a new plugin that's not already installed.
-     *
-     * @param null $metadata
-     */
-
-    // onPluginInstall calls the install schema.
-    /*public static function onPluginInstall(
-        Plugin $plugin,
-        MauticFactory $factory,
-        $metadata = null,
-        $installedSchema = null
-    ): void {
-    }*/
-
-    /**
-     * Called by PluginController::reloadAction when the plugin version does not match what's installed.
-     *
-     * @param null   $metadata
-     * @param Schema $installedSchema
-     *
-     * @throws \Exception
-     */
-    public static function onPluginUpdate(Plugin $plugin, MauticFactory $factory, $metadata = null, Schema $installedSchema = null): void
-    {
-    }
 }
